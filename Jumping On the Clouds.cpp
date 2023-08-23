fe1543b18a99e60726c559e9ff6801d5826b98f1
Vaishnav Sharan <98391200+VaishnavSharan@users.noreply.github.com>
2023-08-23 14:14:51 +0530

    Add files via upload

---
+++ Jumping On the Clouds.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+def jumpingOnClouds(c):
+    x,y = 0,0
+    while x<len(c)-2:
+        x = x+1 if c[x+2] else x+2
+        y+=1
+    if x<len(c)-1:
+        y+=1
+    return y
